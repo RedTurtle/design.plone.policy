a2b7f7c6cc2d581476560a3172e02d9b1cd1efaa
Luca Bellenghi <lucabel@gmail.com>
2020-07-10 16:12:33 +0200

    [fix] fix problem with url passed to frontend. We don't have to return the object physical path, we neet to return the path realtive to the published root site: to do this we can use virtual_url_path

--- src/design/plone/policy/restapi/search_filters/get.py
+++ src/design/plone/policy/restapi/search_filters/get.py
@@ -4,7 +4,7 @@ from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
 from plone import api
 
-SECTION_IDS = ['amministrazione', 'servizi', 'novita', 'documenti-e-dati']
+SECTION_IDS = ["amministrazione", "servizi", "novita", "documenti-e-dati"]
 
 
 @implementer(IPublishTraverse)
@@ -14,29 +14,30 @@ class SearchFiltersGet(Service):
 
     def get_basic_data(self, item):
         return {
-            '@id': item.absolute_url(),
-            'title': item.Title(),
-            'description': item.description,
-            '@type': item.portal_type,
-            'path': '/'.join(item.getPhysicalPath()),
+            "@id": item.absolute_url(),
+            "title": item.Title(),
+            "description": item.description,
+            "@type": item.portal_type,
+            "path": item.virtual_url_path(),
         }
 
     def reply(self):
         portal_path = "/".join(api.portal.get().getPhysicalPath())
         sections = {}
         topics = []
+
         for section_id in SECTION_IDS:
-            section_path = '{portal}/{id}'.format(
+            section_path = "{portal}/{id}".format(
                 portal=portal_path, id=section_id
             )
             section = api.content.get(section_path)
             sections[section_id] = self.get_basic_data(item=section)
-            sections[section_id]['items'] = []
+            sections[section_id]["items"] = []
             for children in section.listFolderContents():
-                sections[section_id]['items'].append(
+                sections[section_id]["items"].append(
                     self.get_basic_data(item=children)
                 )
         for argument in api.content.find(portal_type="Pagina Argomento"):
             topics.append(self.get_basic_data(argument.getObject()))
-        res = {'sections': sections, 'topics': topics}
+        res = {"sections": sections, "topics": topics}
         return res
