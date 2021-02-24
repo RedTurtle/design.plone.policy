8f463568f0805f35180b927e1848f82150706689
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-02-24 14:42:22 +0100

    fix test

--- src/design/plone/policy/tests/test_search_filters_api.py
+++ src/design/plone/policy/tests/test_search_filters_api.py
@@ -40,31 +40,18 @@ class SearchFiltersAPITest(unittest.TestCase):
     def test_endpoint_return_list_of_sections(self):
         response = self.api_session.get("/@search-filters").json()
 
-        self.assertIn('sections', response)
-        sections = response['sections']
-        self.assertIn('amministrazione', sections)
-        self.assertIn('documenti-e-dati', sections)
-        self.assertIn('novita', sections)
-        self.assertIn('servizi', sections)
+        self.assertIn("sections", response)
+        sections = response["sections"]
+
+        self.assertEqual(len(sections), 1)
+        self.assertEqual(len(sections[0]["items"]), 4)
+        self.assertEqual("Amministrazione", sections[0]["items"][0]["title"])
+        self.assertEqual("Servizi", sections[0]["items"][1]["title"])
+        self.assertEqual("Novità", sections[0]["items"][2]["title"])
+        self.assertEqual("Documenti e dati", sections[0]["items"][3]["title"])
 
     def test_endpoint_return_list_of_topics_empty_if_no_topics(self):
         response = self.api_session.get("/@search-filters").json()
 
-        self.assertIn('topics', response)
-        self.assertEqual(response['topics'], [])
-
-    # temporary disabled
-    # def test_endpoint_return_list_of_topics_empty_if_topics(self):
-
-    #     api.content.create(
-    #         container=self.portal, type="Pagina Argomento", title="foo"
-    #     )
-    #     api.content.create(
-    #         container=self.portal, type="Pagina Argomento", title="bar"
-    #     )
-
-    #     commit()
-    #     response = self.api_session.get("/@search-filters").json()
-
-    #     self.assertIn('topics', response)
-    #     self.assertEqual(len(response['topics']), 2)
+        self.assertIn("topics", response)
+        self.assertEqual(response["topics"], [])
