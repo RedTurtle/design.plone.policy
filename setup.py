3f3a6b380377fbe7d70091ffc5784dc5479309f6
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-03-24 12:02:53 +0100

    Back to development: 1.1.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="design.plone.policy",
-    version="1.1.0",
+    version="1.1.1.dev0",
     description="Pacchetto per creare un sito Agid su Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
