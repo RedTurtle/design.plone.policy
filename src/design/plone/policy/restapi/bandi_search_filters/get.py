c03f111f5bcf6b7019ad37e03fa540c1c4e6b908
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-05-31 16:13:46 +0200

    [new] @search-bandi-filters endpoint

--- src/design/plone/policy/restapi/bandi_search_filters/get.py
+++ src/design/plone/policy/restapi/bandi_search_filters/get.py
@@ -1,20 +1,8 @@
 # -*- coding: utf-8 -*-
-from AccessControl.unauthorized import Unauthorized
-from design.plone.contenttypes.controlpanels.settings import (
-    IDesignPloneSettings,
-)
 from plone import api
-from plone.registry.interfaces import IRegistry
-from plone.restapi.interfaces import ISerializeToJsonSummary
 from plone.restapi.services import Service
-from Products.CMFPlone.interfaces import ISearchSchema
-from zope.component import getMultiAdapter
-from zope.component import getUtility
-from zope.i18n import translate
 from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
-
-import json
 
 
 @implementer(IPublishTraverse)
