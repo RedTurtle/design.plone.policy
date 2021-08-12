edaa7e77b1c63ec0d861ca739f98c824e68d0d98
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-08-12 14:20:06 +0200

    add new vocabs

--- src/design/plone/policy/enabled_vocabularies.py
+++ src/design/plone/policy/enabled_vocabularies.py
@@ -5,4 +5,6 @@ def allowed_vocabularies():
     return [
         "design.plone.vocabularies.tipologie_unita_organizzativa",
         "design.plone.vocabularies.uo_locations",
+        "redturtle.bandi.tipologia.vocabulary",
+        "redturtle.bandi.enti.vocabulary",
     ]
