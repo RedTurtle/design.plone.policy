e35f2a75aa15f0a928da3ff912be0debbb9f4643
Andrea Cecchi <andrea.cecchi85@gmail.com>
2021-12-01 16:30:17 +0100

    remove customization

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: Addon",
