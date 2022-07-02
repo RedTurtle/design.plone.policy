3fad8fec4faafff1c0510ce8473bb9107afc8f85
Andrea Cecchi <andrea.cecchi85@gmail.com>
2022-07-02 15:16:48 +0200

    fix logic

--- src/design/plone/policy/restapi/bandi_search_filters/get.py
+++ src/design/plone/policy/restapi/bandi_search_filters/get.py
@@ -8,17 +8,18 @@ from zope.publisher.interfaces import IPublishTraverse
 @implementer(IPublishTraverse)
 class BandiSearchFiltersGet(Service):
     def reply(self):
-        subjects = [
-            {"UID": x, "title": x}
-            for x in self.context.portal_catalog.uniqueValuesFor("Subject_bando")
-        ]
+        """
+        Return possible values based also on current user permissions
+        """
+        subjects = []
+        pc = api.portal.get_tool(name="portal_catalog")
+        for subject in pc.uniqueValuesFor("Subject_bando"):
+            res = api.content.find(Subject_bando=subject)
+            if res:
+                subjects.append({"UID": subject, "title": subject})
 
-        uffici_uids = self.context.portal_catalog.uniqueValuesFor(
-            "ufficio_responsabile_bando"
-        )
-        offices = [
-            {"UID": x.UID, "title": x.Title} for x in api.content.find(UID=uffici_uids)
-        ]
+        office_uids = pc.uniqueValuesFor("ufficio_responsabile_bando")
+        offices = [{"UID": x.UID, "title": x.Title} for x in pc(UID=office_uids)]
 
         subjects.sort(key=lambda x: x["title"])
         offices.sort(key=lambda x: x["title"])
