fed77d777a709089b1fbfd39b88b61f73105a2c5
Luca Bellenghi <lucabel@gmail.com>
2023-03-01 12:08:52 +0100

    Back to development: 5.0.0a6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="design.plone.policy",
-    version="5.0.0a5",
+    version="5.0.0a6.dev0",
     description="Pacchetto per creare un sito Agid su Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
