d2a0d96847205d6ab6c721f55fe526e1c35942b8
Mauro Amico <mauro.amico@gmail.com>
2023-05-04 12:32:43 +0200

    black

--- src/design/plone/policy/rejectanonymous.py
+++ src/design/plone/policy/rejectanonymous.py
@@ -4,5 +4,5 @@ from iw.rejectanonymous import rejectAnonymous
 
 def insertRejectAnonymousHook(portal, event):
     """force authentication for request with X-ForceAuth header"""
-    if event.request.getHeader('X-ForceAuth'):
+    if event.request.getHeader("X-ForceAuth"):
         event.request.post_traverse(rejectAnonymous, (portal, event.request))
