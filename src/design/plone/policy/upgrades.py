087f0390067e97f4de1432a42eaaaad8e352da0d
Roman <roman.kysil@redturtle.it>
2023-06-20 17:13:29 +0200

    FIx

--- src/design/plone/policy/upgrades.py
+++ src/design/plone/policy/upgrades.py
@@ -27,7 +27,9 @@ DEFAULT_PROFILE = "profile-design.plone.policy:default"
 
 
 def update_profile(context, profile, run_dependencies=True):
-    context.runImportStepFromProfile(DEFAULT_PROFILE, profile, run_dependencies)
+    context.runImportStepFromProfile(
+        DEFAULT_PROFILE, profile, run_dependencies
+    )
 
 
 def update_types(context):
@@ -64,7 +66,9 @@ def to_1200(context):
     installOrReinstallProduct(api.portal.get(), "collective.volto.formsupport")
     logger.info("Changing form block fields.")
     i = 0
-    brains = api.content.find(object_provides="plone.restapi.behaviors.IBlocks")
+    brains = api.content.find(
+        object_provides="plone.restapi.behaviors.IBlocks"
+    )
     tot = len(brains)
     fixed_items = []
     for brain in brains:
@@ -93,7 +97,9 @@ def to_1300(context):
 
 
 def to_1400(context):
-    old = api.portal.get_registry_record(name="design.plone.policy.twitter_token")
+    old = api.portal.get_registry_record(
+        name="design.plone.policy.twitter_token"
+    )
     context.runAllImportStepsFromProfile("profile-design.plone.policy:to_1400")
     update_registry(context)
 
@@ -174,7 +180,9 @@ def to_1910(context):
             else:
                 new_sizes.append(size)
     if "midi 300:65536" not in new_sizes:
-        new_sizes.insert(new_sizes.index("mini 200:65536") + 1, "midi 300:65536")
+        new_sizes.insert(
+            new_sizes.index("mini 200:65536") + 1, "midi 300:65536"
+        )
     api.portal.set_registry_record("plone.allowed_sizes", new_sizes)
 
 
@@ -235,7 +243,9 @@ def to_2000(context):  # noqa: C901
                         blocks = value.get("blocks", {})
                     except AttributeError:
                         logger.warning(
-                            "[RICHTEXT] - {} (not converted)".format(brain.getURL())
+                            "[RICHTEXT] - {} (not converted)".format(
+                                brain.getURL()
+                            )
                         )
                     if blocks:
                         res = fix_block(blocks)
@@ -263,7 +273,9 @@ def update_folders(context, CHANGES=[], NEW_ITEMS=[]):
     portal_name = portal.getId()
     for item in CHANGES:
         try:
-            folder = portal.restrictedTraverse("{}{}".format(portal_name, item))
+            folder = portal.restrictedTraverse(
+                "{}{}".format(portal_name, item)
+            )
             old_title = folder.title
             old_path = "/".join(folder.getPhysicalPath())
             api.content.rename(obj=folder, new_id=CHANGES[item][1])
@@ -277,7 +289,9 @@ def update_folders(context, CHANGES=[], NEW_ITEMS=[]):
                 )
             )
         except KeyError:
-            logger.info("{} Impossibile modificare {}{}".format(RED, item, ENDC))
+            logger.info(
+                "{} Impossibile modificare {}{}".format(RED, item, ENDC)
+            )
 
     for item in NEW_ITEMS:
         folder = portal.restrictedTraverse("{}{}".format(portal_name, item[0]))
@@ -376,7 +390,7 @@ def to_3101(context):
 
         return blocks
 
-    for brain in api.portal.get("portal_catalog")():
+    for brain in api.portal.get_tool("portal_catalog")():
         item = aq_base(brain.getObject())
         for schema in iterSchemata(item):
             for name, field in getFields(schema).items():
@@ -391,7 +405,9 @@ def to_3101(context):
                         blocks = value.get("blocks", {})
                     except AttributeError:
                         logger.warning(
-                            "[BLOCK] - {} (not converted)".format(brain.getURL())
+                            "[BLOCK] - {} (not converted)".format(
+                                brain.getURL()
+                            )
                         )
                     if blocks:
                         item.blocks = remove_twitter(blocks)
