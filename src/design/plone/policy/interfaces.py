d7992f26bf556a1ba2318636c9ffd16b5ebcb739
Roman <roman.kysil@redturtle.it>
2023-06-20 15:47:52 +0200

    Unused imports remove

--- src/design/plone/policy/interfaces.py
+++ src/design/plone/policy/interfaces.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
-from zope import schema
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
