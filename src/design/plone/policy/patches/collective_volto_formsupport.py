55553a00b74a86f42d31e880f2fa939529d2329c
Andrea Cecchi <andrea.cecchi85@gmail.com>
2025-10-07 13:33:50 +0200

    Fix formsupport 3.3.0 compatibility (in add patch).

--- src/design/plone/policy/patches/collective_volto_formsupport.py
+++ src/design/plone/policy/patches/collective_volto_formsupport.py
@@ -205,8 +205,11 @@ def add(self, data):
         return None
 
     fields = {
-        x["field_id"]: x.get("custom_field_id", x.get("label", x["field_id"]))
-        for x in form_fields
+        f["field_id"]: {
+            "label": f.get("custom_field_id", f.get("label", f["field_id"])),
+            "type": f.get("field_type", "text"),
+        }
+        for f in form_fields
     }
     record = Record()
     fields_labels = {}
@@ -217,7 +220,8 @@ def add(self, data):
         field_id = field_data.get("field_id", "")
         value = field_data.get("value", "")
         if field_id in fields:
-            record.attrs[field_id] = value
+            field = fields[field_id]
+            record.attrs[field_id] = self.storedValue(value, field["type"])
             fields_labels[field_id] = fields[field_id]
             fields_order.append(field_id)
     record.attrs["fields_labels"] = fields_labels
