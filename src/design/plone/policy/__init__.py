a24de0cdcfd6b3000cf412615f4d626a88e82235
Federico Vancini <federico.vancini@redturtle.it>
2026-05-20 13:59:00 +0200

    fix translation locales import order (#87)
    * fix translation locales import order
    
    * zpretty and comment
    
    * Clarify translation loading in initialize function
    
    Updated the docstring in the initialize function to clarify the translation loading order and reference a proposed fix.
    
    * black
    
    ---------
    
    Co-authored-by: Mauro Amico <mauro.amico@gmail.com>

--- src/design/plone/policy/__init__.py
+++ src/design/plone/policy/__init__.py
@@ -3,6 +3,31 @@
 
 from .sensitive import apply
 from zope.i18nmessageid import MessageFactory
+from zope.i18n.interfaces import ITranslationDomain
+from zope.component import queryUtility
+import logging
+
+logger = logging.getLogger(__name__)
 
 _ = MessageFactory("design.plone.policy")
 apply()
+
+
+def initialize(context):
+    """Be sure that we have our translations loaded before the othe ones
+    Fix proposto da @alert dell'ordine delle traduzioni preso da
+    https://community.plone.org/t/overriding-plone-app-locales/12021/14
+    """
+    translation_domain = queryUtility(ITranslationDomain, "plone")
+    if not translation_domain:
+        return
+
+    for lang in translation_domain._catalogs:
+        original = translation_domain._catalogs[lang]
+        # This will enforce our translations to be at the top of the list
+        tweaked = sorted(
+            original, key=lambda path: "design/plone/policy/locales" not in path
+        )
+        if tweaked != original:
+            logger.warning("Boosting this package translations")
+            translation_domain._catalogs[lang] = tweaked
