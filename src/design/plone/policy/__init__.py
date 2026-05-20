496c9f47a6cfce7b9422208dadc9b46c8a5141c3
Federico Vancini <federico.vancini@redturtle.it>
2026-05-20 12:05:19 +0200

    fix translation locales import order

--- src/design/plone/policy/__init__.py
+++ src/design/plone/policy/__init__.py
@@ -3,6 +3,28 @@
 
 from .sensitive import apply
 from zope.i18nmessageid import MessageFactory
+from zope.i18n.interfaces import ITranslationDomain
+from zope.component import queryUtility
+import logging
+
+logger = logging.getLogger(__name__)
 
 _ = MessageFactory("design.plone.policy")
 apply()
+
+
+def initialize(context):
+    """Be sure that we have our translations loaded before the othe ones"""
+    translation_domain = queryUtility(ITranslationDomain, "plone")
+    if not translation_domain:
+        return
+
+    for lang in translation_domain._catalogs:
+        original = translation_domain._catalogs[lang]
+        # This will enforce our translations to be at the top of the list
+        tweaked = sorted(
+            original, key=lambda path: "design/plone/policy/locales" not in path
+        )
+        if tweaked != original:
+            logger.warning("Boosting this package translations")
+            translation_domain._catalogs[lang] = tweaked
